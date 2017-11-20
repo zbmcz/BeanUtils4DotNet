2b5cf223cca72d214afc74c651244b08cdc8486b
zbmcz <pandaball95@gmail.com>
2017-11-20 16:25:48 +0800

    基本功能

---
+++ BeanUtils/BeanUtils.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+namespace Abchina.Ebiz.Tools.BeanUtils
+{
+    /// <summary>
+    /// Utility methods for populating Beans properties via reflection.
+    /// </summary>
+    public class BeanUtils
+    {
+        
+        public BeanUtils()
+        {
+        }
+
+        /// <summary>
+        /// Clone a bean based on the available property via accessors.
+        /// </summary>
+        /// <returns>The bean.</returns>
+        /// <param name="bean">Bean.</param>
+        /// <param name="constructorArgs">Constructor arguments.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static T CloneBean<T>(Object bean,Object[] constructorArgs = null)
+        {
+            Type beanType = bean.GetType();
+            Type[] constructorArgTypes = Type.EmptyTypes;
+
+            if (constructorArgs != null){
+                constructorArgTypes = new Type[constructorArgs.Length];
+                for (int i = 0; i < constructorArgs.Length; i++){
+                    constructorArgTypes[i] = constructorArgs[i].GetType();
+                }
+            }
+
+            // get the specified constructor of bean
+            ConstructorInfo constructor = beanType.GetConstructor(constructorArgTypes);
+
+            // get the new instance of bean
+            Object ret = constructor.Invoke(constructorArgs);
+
+            PropertyInfo[] publicProps = beanType.GetProperties();
+            foreach (PropertyInfo propInfo in publicProps)
+            {
+                propInfo.SetValue(ret,propInfo.GetValue(bean));
+            }
+            return (T)ret;
+        }
+
+        /// <summary>
+        /// Copy property values from the origin bean to the destination bean for all cases where the property names are the same.
+        /// </summary>
+        /// <param name="dest">Destination.</param>
+        /// <param name="origin">Origin.</param>
+        public static void CopyPorperties(Object dest,Object origin)
+        {
+            Type beanType = dest.GetType();
+            PropertyInfo[] propInfos = beanType.GetProperties();
+            foreach(var prop in propInfos)
+            {
+                prop.SetValue(dest,prop.GetValue(origin));
+            }
+        }
+
+        /// <summary>
+        /// Copy the specified property value to the specified destination bean, performing any type conversion that is required.
+        /// </summary>
+        /// <param name="bean">Bean.</param>
+        /// <param name="name">Name.</param>
+        /// <param name="value">Value.</param>
+        public static void CopyProperty(Object bean,string name,Object value)
+        {
+            Type beanType = bean.GetType();
+            PropertyInfo propInfo = beanType.GetProperty(name);
+            propInfo.SetValue(bean,value);
+        }
+
+        /// <summary>
+        /// Return the entire collection of properties for which the specified bean provides a read accessor.
+        /// </summary>
+        /// <returns>The describe.</returns>
+        /// <param name="bean">Bean.</param>
+        public static Dictionary<string,Object> Describe(Object bean)
+        {
+            PropertyInfo[] propInfos = bean.GetType().GetProperties();
+            Dictionary<string, Object> ret = new Dictionary<string, object>();
+            foreach (var propInfo in propInfos)
+            {
+                ret.Add(propInfo.Name,propInfo.GetValue(bean));
+            }
+            return ret;
+        }
+
+    }
+}
---
+++ BeanUtils/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abchina.Ebiz.Tools.BeanUtils.Test
+{
+    class MainClass
+    {
+        /// <summary>
+        /// TestCase BeanUtils
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            Student stu = new Student();
+            stu.Name = "Giraffe";
+            stu.Company = "ABC";
+            stu._school = "HEBUT";
+
+            Student stu1 = BeanUtils.CloneBean<Student>(stu,new object[1]{"BeiDa"});
+            Console.WriteLine(stu1.ToString());
+
+            BeanUtils.CopyProperty(stu1,"Company","H3C");
+            Console.WriteLine(stu1.ToString());
+
+            Student stu2 = new Student();
+            BeanUtils.CopyPorperties(stu2,stu1);
+            Console.WriteLine(stu2.ToString());
+
+            Dictionary<string,object> dic = BeanUtils.Describe(stu2);
+            foreach(var item in dic){
+                Console.WriteLine("----" + item.Key + "----" + item.Value + "----");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    class Student
+    {
+
+        public string Name{
+            get;set;
+        }
+        private string Sex{
+            get;set;
+        }
+
+        private string _company;
+        public string Company { 
+            get{
+                return this._company;
+            }
+            set{
+                this._company = value;
+            }
+        }
+
+        public string _school;
+
+        public override string ToString()
+        {
+            return string.Format("[Student: Name={0}, Company={1} , _school={2}]", Name, Company,this._school);
+        }
+
+
+        public Student(string school){
+            this._school = school;
+        }
+        public Student(){
+            
+        }
+
+    }
+}
